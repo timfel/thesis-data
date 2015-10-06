1d0514fb95cc6153925ac785b0ae4de68ce3488b
Tim Felgentreff <timfelgentreff@gmail.com>
2015-10-06 10:39:49 +0200

    fix smm/js script

--- benchmark-constraint-languages/sendmoremoney.js
+++ benchmark-constraint-languages/sendmoremoney.js
@@ -16,34 +16,35 @@ function action() {
     setTimeout(function () {
         var start = Date.now();
         var obj = {s: 0, e: 0, n: 0, d: 0, m: 0, o: 0, r: 0, y:0};
+        var names = ["s", "e", "n", "d", "m", "o", "r", "y"];
         var fo;
-        for (var f in obj) {
+        names.forEach(function (f) {
             bbb.always({
                 solver: solver,
-                ctx: {obj: obj, f: f, fo: fo}}, function () {
+                ctx: {obj: obj, f: f}}, function () {
                     return obj[f] <= 9 && obj[f] >= 0 });
-            for (var fo in obj) {
+        });
+        bbb.always({
+            solver: solver,
+            ctx: {obj: obj}}, function () {
+                return obj.s != 0 && obj.m != 0 });
+        names.forEach(function (f) {
+            names.forEach(function (fo) {
                 if (f !== fo) {
                     bbb.always({
                         solver: solver,
                         ctx: {obj: obj, f: f, fo: fo}}, function () {
                             return obj[f] != obj[fo] });
                 }
-            }
-        }
-
+            })
+        })
 
         bbb.always({
             solver: solver,
             ctx: {obj: obj}}, function () {
-                obj.s * 1000 + obj.e * 100 + obj.n * 10 + obj.d +
+                return obj.s * 1000 + obj.e * 100 + obj.n * 10 + obj.d +
                     obj.m * 1000 + obj.o * 100 + obj.r * 10 + obj.e ==
                     obj.m * 10000 + obj.o * 1000 + obj.n * 100 + obj.e * 10 + obj.y });
-
-        bbb.always({
-            solver: solver,
-            ctx: {obj: obj}}, function () {
-                obj.s > 0 && obj.m > 0 });
 
         console.log("solution: [s,e,n,d,m,o,r,y] = " + [obj.s,obj.e,obj.n,obj.d,obj.m,obj.o,obj.r,obj.y])
         TIME_ += (Date.now() - start);
